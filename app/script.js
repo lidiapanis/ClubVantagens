e1fabbcfdd428c95e4eb2d08b2660394bda6e469
Lidia Panis <lpse@live.com>
2022-07-28 16:51:06 -0300

    Add files via upload

--- app/script.js
+++ app/script.js
@@ -8,9 +8,9 @@ const aplicacao = new Vue({
         { id: 3, nome: 'Promoções de pontuação extra', descricao: "Ganhe pontos extras participando de aceleradores sazonais"},
       ],
       colecaoNiveis: [
-        { id: 1, nivel: "1º nível - Prata", descricao: "Combo prata, Desconto de 50% em ingresso inteira, ingresso de aniversário, ingresso gratis a cada 200 pontos" },
-        { id: 2, nivel: "2º nível - Ouro", descricao: "Combo ouro, desconto de 50% em ingresso inteira, ingresso e combo de aniversário, ingresso gratis a cada 200 pontos" },
-        { id: 3, nivel: "3º nível - Diamante", descricao: "Combo diamante, desconto de 50% em ingresso inteira, ingressos e combo de aniversário, ingresso gratis a cada 200 pontos" },
+        { id: 1, nivel: "1º nível - Prata", descricao1: "Combo prata", descricao2: "Desconto em ingresso inteira*", descricao3: "Ingresso de aniversário", descricao4:"Ingresso gratis a cada 200 pontos" },
+        { id: 2, nivel: "2º nível - Ouro", descricao1: "Combo ouro", descricao2: "Desconto em ingresso inteira*", descricao3: "Ingresso e combo de aniversário", descricao4:"Ingresso gratis a cada 200 pontos" },
+        { id: 3, nivel: "3º nível - Diamante", descricao1: "Combo diamante", descricao2: "Desconto em ingresso inteira", descricao3:"Ingressos e combo de aniversário", descricao4:"Ingresso gratis a cada 200 pontos" },
       ],
     }
   });
