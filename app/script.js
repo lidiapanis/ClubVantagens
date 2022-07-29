d8217a5345f83bad7665a6c8e2522834385d007f
Lidia Panis <lpse@live.com>
2022-07-29 11:52:52 -0300

    Alterações de lista

--- app/script.js
+++ app/script.js
@@ -8,9 +8,9 @@ const aplicacao = new Vue({
         { id: 3, nome: 'Promoções de pontuação extra', descricao: "Ganhe pontos extras participando de aceleradores sazonais"},
       ],
       colecaoNiveis: [
-        { id: 1, nivel: "1º nível - Prata", descricao1: "Combo prata", descricao2: "Desconto em ingresso inteira*", descricao3: "Ingresso de aniversário", descricao4:"Ingresso gratis a cada 200 pontos" },
-        { id: 2, nivel: "2º nível - Ouro", descricao1: "Combo ouro", descricao2: "Desconto em ingresso inteira*", descricao3: "Ingresso e combo de aniversário", descricao4:"Ingresso gratis a cada 200 pontos" },
-        { id: 3, nivel: "3º nível - Diamante", descricao1: "Combo diamante", descricao2: "Desconto em ingresso inteira", descricao3:"Ingressos e combo de aniversário", descricao4:"Ingresso gratis a cada 200 pontos" },
+        { id: 1, nivel: "1º nível - Prata", descricao1: "Combo prata", descricao2: "Desconto em ingresso inteira na Segunda-feira", descricao3: "1 Ingresso de aniversário", descricao4:"Ingresso gratis a cada 200 pontos" },
+        { id: 2, nivel: "2º nível - Ouro", descricao1: "Combo ouro", descricao2: "Desconto em ingresso inteira na Segunda e Quarta-feira", descricao3: "1 Ingresso e 1 combo de aniversário", descricao4:"Ingresso gratis a cada 200 pontos" },
+        { id: 3, nivel: "3º nível - Diamante", descricao1: "Combo diamante", descricao2: "Desconto em ingresso inteira de Segunda a Quarta-feira", descricao3:"2 Ingressos e 1 combo de aniversário", descricao4:"Ingresso gratis a cada 200 pontos" },
       ],
     }
   });
